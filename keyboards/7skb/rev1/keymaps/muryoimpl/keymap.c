edbb00990f27889942631cd8dd1ba4de64bddc98
muryoimpl <muryoimpl@gmail.com>
2020-12-07 00:02:40 +0900

    Assign Right heart for Ctrl-Q

--- keyboards/7skb/rev1/keymaps/muryoimpl/keymap.c
+++ keyboards/7skb/rev1/keymaps/muryoimpl/keymap.c
@@ -32,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------+--------+--------|
      KC_LCTRL,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,        KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,  KC_ENT,
   //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------+--------|
-      KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,        KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT, TG(_QWERTY),
+      KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,        KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT, RCTL(KC_Q),
   //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------+--------|
                TG(_FN), KC_LGUI,  KC_LALT,  KC_SPC,               KC_SPC, MO(_FNC),          KC_RALT, S(LWIN(SWIN(KC_DEL)))
           //`---------------------------------------------|   |--------------------------------------------'
