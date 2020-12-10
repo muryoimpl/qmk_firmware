1486edfb0141d25ed0d54d5d5b562fe93c02b79a
muryoimpl <muryoimpl@gmail.com>
2020-12-11 01:12:20 +0900

    Assign ESC to ~ in FUNC layer

--- keyboards/7skb/rev1/keymaps/muryoimpl/keymap.c
+++ keyboards/7skb/rev1/keymaps/muryoimpl/keymap.c
@@ -70,7 +70,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_FNC] = LAYOUT(
   //,-----------------------------------------------------|   |--------------------------------------------------------------------------------.
-      XXXXXXX, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,       KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  XXXXXXX, KC_DEL,
+      KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,       KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  XXXXXXX, KC_DEL,
   //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------+--------+--------+--------|
       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     KC_PGUP, KC_HOME, KC_UP,   KC_END,  KC_DEL, KC_PSCR, XXXXXXX,  RESET,
   //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------+--------+--------|
