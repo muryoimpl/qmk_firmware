31cd770dd05e053e4145a898b0a63381b3d7c521
Ken Muryoi <k-muryoi@esm.co.jp>
2021-01-20 17:06:26 +0900

    Update zinc:muryoimpl keymaps

--- keyboards/zinc/keymaps/muryoimpl/keymap.c
+++ keyboards/zinc/keymaps/muryoimpl/keymap.c
@@ -11,14 +11,12 @@ enum layer_number {
     _QWERTY = 0,
     _LOWER,
     _RAISE,
-    _ADJUST
 };
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
   LOWER,
   RAISE,
-  ADJUST,
   MCR0,
   MCR1,
   MCR2,
@@ -32,7 +30,6 @@ enum macro_keycodes {
 #define M_SAMPLE M(KC_SAMPLEMACRO)
 #define M_XSC S(LWIN(SWIN(KC_DEL)))
 
-#define ADJ _ADJUST
 #define RAI _RAISE
 #define LOW _LOWER
 
@@ -45,14 +42,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = { \
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
    * | Shift|   Z  |   X  |   C  |   V  |   B  |             |   N  |   M  |   ,  |   .  |   /  |Enter |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-   * | Esc  | WIN  |MO(AD)| Alt  |   ~  |Space |             |MO(LO)|Space |MO(RA)| Shift|   [  |  ]   |
+   * | Esc  | Alt  | WIN  |  -   |   ~  |Space |             |MO(LO)|Space |MO(RA)| Shift|   [  |  ]   |
    * `-----------------------------------------'             `-----------------------------------------'
    */
 [_QWERTY] = LAYOUT_ortho_4x12(
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
       KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
       KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
-      KC_ESC,  KC_LGUI, MO(ADJ), KC_LALT, KC_GRV, KC_SPC,                     MO(LOW), KC_SPC,  MO(RAI), KC_RSFT, KC_LBRC, KC_RBRC \
+      KC_ESC, KC_LALT,  KC_LGUI, KC_MINS, KC_GRV, KC_SPC,                     MO(LOW), KC_SPC,  MO(RAI), KC_RSFT, KC_LBRC, KC_RBRC \
       ),
 
   /* Lower
@@ -61,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = { \
    * ,-----------------------------------------.             |------+------+------+------+------+------|
    * |      |   &  |   ^  |   (  |   )  |  _   |             |PageDN| LEFT | DOWN | RIGHT|      |      |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-   * |      |   +  |   -  |   *  |   /  |  =   |             | MCR0 | MCR1 | MCR2 |      |      |      |
+   * |      |   \  |   -  |   *  |   /  |  =   |             | MCR0 | MCR1 | MCR2 |      |      |      |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
    * |      |      |      |      |      |      |             |      |      |      |      |   [  |   ]  |
    * `-----------------------------------------'             `-----------------------------------------'
@@ -69,7 +66,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = { \
   [_LOWER] = LAYOUT_ortho_4x12( \
       _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_PGUP, KC_HOME, KC_UP,   KC_END,  _______, KC_DEL, \
       _______, KC_AMPR, KC_CIRC, KC_LPRN, KC_RPRN, KC_UNDS,                   KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, \
-      _______, KC_PLUS, KC_MINS, KC_PAST, KC_SLSH, KC_EQL,                    _______, _______, _______, _______, _______, _______, \
+      _______, KC_BSLS, KC_MINS, KC_PAST, KC_SLSH, KC_EQL,                    MCR0,    MCR1,    MCR2,    _______, _______, _______, \
       _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______ \
       ),
 
@@ -79,35 +76,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = { \
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
    * |      |   6  |   7  |   8  |   9  |   0  |             |  F6  |  F7  |  F8  |  F9  |  F10 | F11  |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-   * |      |   +  |   -  |   *  |   /  |   =  |             |  F12 |      |      |      |      |      |
+   * |      |   \  |   -  |   *  |   /  |   =  |             |  F12 |      |      |      |      |      |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-   * |      |      |      |      |      |      |             |      |      |      |M_XSC |      |      |
+   * |      |      |      |      |      |      |             |      |      |      |M_XSC |      |RESET |
    * `-----------------------------------------'             `-----------------------------------------'
    */
   [_RAISE] = LAYOUT_ortho_4x12( \
-      KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,    KC_DEL, \
-      _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,                      KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11, \
-      _______, KC_PLUS, KC_MINS, KC_F9,   KC_F10,  KC_F11,                    KC_F12,  _______, _______, _______, _______, _______, \
-      _______, _______, _______, _______, _______, _______,                   _______, _______, _______, M_XSC,   _______, _______ \
+      KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_DEL, \
+      _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,                      KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, \
+      _______, KC_BSLS, KC_MINS, KC_PAST, KC_SLSH, KC_EQL,                    KC_F12,  _______, _______, _______, _______, _______, \
+      _______, _______, _______, _______, _______, _______,                   _______, _______, _______, M_XSC,   _______, RESET \
       ),
-
-  /* Adjust (Lower + Raise)
-   * ,-----------------------------------------.             ,-----------------------------------------.
-   * |      | Reset|RGBRST|Aud on|Audoff|      |             |      |Qwerty|Colemk|Dvorak|      | Ins  |
-   * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-   * |      |RGB ON| HUE+ | SAT+ | VAL+ | Mac  |             | Win  |  -   |   =  |Print |ScLock|Pause |
-   * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-   * |MODE R| MODE | HUE- | SAT- | VAL- |      |             |      |      |      |      |PageUp|      |
-   * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-   * |      |      |      | EISU | EISU | EISU |             | KANA | KANA | Home |PageDn|PageUp| End  |
-   * `-----------------------------------------'             `-----------------------------------------'
-   */
-    [_ADJUST] =  LAYOUT_ortho_4x12( \
-      _______, RESET,   _______, _______, _______, _______,                   _______, QWERTY,  _______, _______,  _______, KC_INS, \
-      _______, _______, _______, _______, _______, _______,                   AG_SWAP, KC_MINS, KC_EQL,  KC_PSCR, KC_SLCK, KC_PAUS,\
-      _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, KC_PGUP, _______,\
-      _______, _______, _______, _______, _______, _______,                   _______,    _______,    _______,    KC_HOME, KC_PGDN, KC_END\
-      )
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -137,15 +116,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         layer_on(_RAISE);
       } else {
         layer_off(_RAISE);
-      }
-      return false;
-      break;
-
-    case ADJUST:
-      if (record->event.pressed) {
-        layer_on(_ADJUST);
-      } else {
-        layer_off(_ADJUST);
       }
       return false;
       break;
